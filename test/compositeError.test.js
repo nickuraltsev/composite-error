9e79c7e9bd2938687e0d2e9d3f4d7e8b382aea0e
Nick Uraltsev <nick.uraltsev@gmail.com>
2015-06-15 22:33:33 -0700

    Add more tests

--- test/compositeError.test.js
+++ test/compositeError.test.js
@@ -14,7 +14,7 @@ describe('CompositeError', function() {
                                 new Error('first error')));
                     return error.stack;
                 });
-                stack.should.be.equal(
+                stack.should.equal(
                     '<stack trace: third error>' +
                     '\n--- Inner error: <stack trace: second error>' +
                     '\n--- Inner error: <stack trace: first error>' +
@@ -22,6 +22,7 @@ describe('CompositeError', function() {
                     '\n--- End of inner error');
             });
         });
+
         describe('when there is more than one inner error', function() {
             it('should contain stack traces of current error and all its inner errors', function() {
                 var stack = usingMockStackTraceFormatter(function() {
@@ -31,12 +32,48 @@ describe('CompositeError', function() {
                     ]);
                     return error.stack;
                 });
-                stack.should.be.equal(
+                stack.should.equal(
                     '<stack trace: third error>' +
                     '\n--- Inner error #0: <stack trace: first error>' +
                     '\n--- End of inner error' +
                     '\n--- Inner error #1: <stack trace: second error>' +
                     '\n--- End of inner error');
+            });
+        });
+
+        describe('when there are no inner errors', function() {
+            it('should contain stack trace of current error', function() {
+                var stack = usingMockStackTraceFormatter(function() {
+                    var error = new CompositeError('error');
+                    return error.stack;
+                });
+                stack.should.equal('<stack trace: error>');
+            });
+        });
+    });
+
+    describe('#innerErrors', function() {
+        describe('when there is one inner error', function() {
+            it('should contain array of one inner error', function() {
+                var innerError = new Error('inner error');
+                var error = new CompositeError('error', innerError);
+                error.innerErrors.should.eql([innerError]);
+            });
+        });
+
+        describe('when there is more than one inner error', function() {
+            it('should contain array of all inner errors', function() {
+                var innerError1 = new Error('inner error 1');
+                var innerError2 = new Error('inner error 2');
+                var error = new CompositeError('error', [innerError1, innerError2]);
+                error.innerErrors.should.eql([innerError1, innerError2]);
+            });
+        });
+
+        describe('when there are no inner errors', function() {
+            it('should contain empty array', function() {
+                var error = new CompositeError('error');
+                error.innerErrors.should.be.empty.Array;
             });
         });
     });
@@ -47,7 +84,7 @@ describe('CompositeError', function() {
                 var error = new CompositeError('second error', new Error('first error'));
                 return error.ownStack;
             });
-            ownStack.should.be.equal('<stack trace: second error>');
+            ownStack.should.equal('<stack trace: second error>');
         });
     });
 });
